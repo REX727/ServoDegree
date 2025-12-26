d8e4e96583e01aac4a5bf670e8523bcfcdf0fb53
KWONG CHING KWAN <81423069+REX727@users.noreply.github.com>
2025-12-26 20:38:53 +0800

    V0.2.1
    Usable Version
    Fully functional – save and run as a `.py` file.
    GUI or additional interface improvements coming in future updates.

--- ServoDegree.py
+++ ServoDegree.py
@@ -4,22 +4,40 @@ RED = '\033[31m'
 GREEN = '\033[32m'
 END = '\033[0m'
 
-
-ServoD = int(input("Enter servo degree: "))
-
+GBOLD = BOLD + GREEN
+RBOLD = BOLD + RED
 
 while True:
-    if ServoD > 512:
-        ServoDC = ServoD - 512
-        ServoDCon = 512 - ServoDC
-        print(f"Servo Degree after converted: {BOLD}{GREEN}{ServoDCon}{END}")
-    elif ServoD < 512:
-        ServoDC = 512 - ServoD
-        ServoDCon = 512 + ServoDC
-        print(f"Servo Degree after converted: {BOLD}{GREEN}{ServoDCon}{END}")
-    elif ServoD == 512:
-        ServoDCon = 512
-        print(f"Servo Degree after converted: {BOLD}{GREEN}{ServoDCon}{END}")
+    UserInput = input(f"Keep the halt value at {GBOLD}512{END}? (press Enter or n)(or Exit):")
+    if UserInput.lower() == "n":
+        HaltValue = int(input("Enter Halt Value: "))
+    elif UserInput == "":
+        HaltValue = 512
+    elif UserInput.lower() == "exit":
+        print(f"============={RBOLD}Exiting Program.{END}============")
+        break  
     else:
-        print("Invalid input for servo degree.")
-    ServoD = int(input("Enter servo degree: "))
+        print(RED + BOLD + "Error in Halt Value Input" + END)
+        exit()
+
+    ServoValue = int(input(f"Enter {BOLD}Servo Value{END}: "))
+
+
+    if ServoValue > HaltValue:
+        move = ServoValue - HaltValue
+        ServoMoveA = HaltValue - move
+        ServoMoveB = HaltValue + move
+        print(f"\n==========HALT VALUE: {BOLD}{HaltValue}{END} ==========\nServoMoveA: {GBOLD}{ServoMoveA}{END}(Servo Move: -{move})\nServoMoveB: {RBOLD}{ServoMoveB}{END}(Servo Move: +{move})\n")
+    elif ServoValue < HaltValue:    
+        move = HaltValue - ServoValue
+        ServoMoveA = HaltValue - move
+        ServoMoveB = HaltValue + move
+        print(f"\n==========HALT VALUE: {BOLD}{HaltValue}{END} ==========\nServoMoveA: {RBOLD}{ServoMoveA}{END}(Servo Move: +{move})\nServoMoveB: {GBOLD}{ServoMoveB}{END}(Servo Move: -{move})\n")
+    elif ServoValue == HaltValue:
+        ServoMoveA = HaltValue
+        ServoMoveB = HaltValue
+        print(f"\n==========HALT VALUE: {BOLD}{HaltValue}{END} ==========\nServoMoveA: {GBOLD}{ServoMoveA}{END}(No Move)\nServoMoveB: {GBOLD}{ServoMoveB}{END}(No Move)\n")
+    else:
+        print(RED + BOLD + "Error in Servo Degree Calculation" + END)
+
+
